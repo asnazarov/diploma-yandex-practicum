116493bc5cebebc42851203d0d11f4896a132a50
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-07-22 00:15:46 +0300

    собрали webpack

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "../pages/style.css"
+// import Api from "./Api.js";
+
+(function() {
+    const title = document.querySelector('div')
+    const root = document.querySelector('.root');
+
+})();
